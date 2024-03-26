b3990c915a7fb87d581eed6c5c3fef9fbca2a6e7
chromecruzer <147481419+chromecruzer@users.noreply.github.com>
2024-03-26 15:43:44 +0530

    Spinner 🔥

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,13 @@
 # This script echoes "Hello, World!" and then enters an infinite loop
 echo "Hello, World!"
 
+# Display the contents of /etc/os-release
+cat /etc/os-release
+
 # Run an infinite loop to keep the container running
 #while true; do
   #  sleep 1
 #done
+
+# Use tail to keep the container running
+tail -f /dev/null
